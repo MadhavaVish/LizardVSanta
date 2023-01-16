77becf463401c513e685d1b739b1c175c8781512
Madhava Vishnubhotla <madhavavsih@gmail.com>
2023-01-16 13:51:59 +0100

    path finding using mass subsystem

---
+++ Source/LizardVSanta/Public/PathFindingSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "PathFindingSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIZARDVSANTA_API UPathFindingSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
