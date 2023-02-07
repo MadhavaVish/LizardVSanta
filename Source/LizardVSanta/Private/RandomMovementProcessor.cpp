8453395a7e7dc26d2f53a722dd56706f61db0936
6606040 <6606040>
2023-02-07 13:31:39 +0100

    Amount of agents having spawned

--- Source/LizardVSanta/Private/RandomMovementProcessor.cpp
+++ Source/LizardVSanta/Private/RandomMovementProcessor.cpp
@@ -61,8 +61,8 @@ void URandomMovementProcessor::Execute(FMassEntityManager& EntityManager, FMassE
 				//MoveTarget.Center = FVector(6000.f, 0.f, 1.f);
 				MoveTarget.DistanceToGoal = (MoveTarget.Center - Transform.GetLocation()).Size();
 				MoveTarget.Forward = (MoveTarget.Center - Transform.GetLocation()).GetSafeNormal();
-				MoveTarget.Center = PathFinding->GetNextPathPoint(CurrentLocation, FVector(15000.f, 15000.f, -100.f));
-			
+				MoveTarget.Center = PathFinding->GetNextPathPoint(CurrentLocation, FVector(11000.f, 11000.f, -100.f));
+
 			}
 			
 		});
